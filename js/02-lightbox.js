385a7202112661440eb426b99d1fcf03f7025270
VanyaUzhorod <131401354+VanyaUzhorod@users.noreply.github.com>
2023-09-12 21:08:42 +0300

    required

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+
+
+const galleryMarkup = galleryItems.map(item =>`
+<li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      alt="${item.description}"
+     
+    />
+  </a>
+</li>
+`).join('');
+
+galleryList.innerHTML = galleryMarkup;
+
+const Lightbox = new SimpleLigtbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay:250,
+});
